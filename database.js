e57e4e9477be28e979f3b2695b588dbd6e1d16e1
Nex996 <neks96@gmail.com>
2022-11-06 18:56:29 +0100

    New version

--- database.js
+++ database.js
@@ -1,9 +1,35 @@
-let db=[];
+let users=[
+    {
+        id:123,
+        idCardNumber: "00657854" ,
+        name:"Nemanja",
+        lastName:"Novakovic",
+        phoneNumber: "0644833003",
+        roomType:"1 solo bed",
+        startDate: "20-05-2022",
+        endDate:"24-05-2022"
+
+    },
+    { 
+        id:54645,
+        idCardNumber: "009668096" ,
+        name:"Emilia",
+        lastName:"Smith",
+        phoneNumber: "0655257957",
+        roomType:"King size bed",
+        startDate: "02-11-2022",
+        endDate:"14-10-2022"
+    }
+
+];
 
 
 
-let roomType=["1 solo bed", "2 solo bed", "king size bed"];
 
-if(localStorage.db){
-    db = JSON.parse(localStorage.db);
+let roomTypes= ["1 solo bed", "2 solo beds", "King size bed"]
+
+
+if(localStorage.db.length>1){
+    users=JSON.parse(localStorage.db)
+  
 }
--- main.js
+++ main.js
@@ -1,235 +1,287 @@
-window.addEventListener('beforeunload',save);
+//Save users to localStorage
+window.addEventListener("beforeunload", saveToLocalStorage);
 
+//Search
+let searchField = document.querySelector('[type="search"]');
 
-let tbodyUsersView = document.querySelector("#tbodyUsersView");
-let tbodyEditUserView=document.querySelector('#tbodyEditUserView');
-//VIEWS
+//Table
+let usersTable = usersView.querySelector("tbody");
+let editDeleteTable = editDeleteView.querySelector("tbody");
+
+//Views
 let views = document.querySelectorAll(".view");
 
-//INPUTS RENT SAVE
-let inputUserIDCard=document.querySelector("#inputUserIDCard");
-let inputUser=document.querySelector("#inputUser");
-let inputUserPhone=document.querySelector('#inputUserPhone');
-let selectRoomType=document.querySelector("#selectRoomType")
-let inputStartDate=document.querySelector('#inputStartDate');
-let inputEndDate=document.querySelector('#inputEndDate');
-// INPUT EDIT
-let einputUserIDCard=document.querySelector("#einputUserIDCard");
-let einputUser=document.querySelector("#einputUser");
-let einputUserPhone=document.querySelector('#einputUserPhone');
-let eselectRoomType=document.querySelector("#eselectRoomType")
-let einputStartDate=document.querySelector('#einputStartDate');
-let einputEndDate=document.querySelector('#einputEndDate');
+//Rent form inputs
+let idRent = document.querySelector("#idRent");
+let IDcardNumberRent = document.querySelector("#IDcardNumberRent");
+let firstNameRent = document.querySelector("#firstNameRent");
+let lastNameRent = document.querySelector("#lastNameRent");
+let phoneNumberRent = document.querySelector("#phoneNumberRent");
+let roomTypeSelectRent = document.querySelector("#roomTypeSelectRent");
+let startDateRent = document.querySelector("#startDateRent");
+let endDateRent = document.querySelector("#endDateRent");
 
+//Edit form inputs
+let eIDcardNumberRent = document.querySelector("#eIDcardNumberRent");
+let efirstNameRent = document.querySelector("#efirstNameRent");
+let elastNameRent = document.querySelector("#elastNameRent");
+let ephoneNumberRent = document.querySelector("#ephoneNumberRent");
+let eroomTypeSelectRent = document.querySelector("#eroomTypeSelectRent");
+let estartDateRent = document.querySelector("#estartDateRent");
+let eendDateRent = document.querySelector("#eendDateRent");
 
+//Nav links
+let rentNavLink = document.querySelector(".rentNavLink-js");
+let usersNavLink = document.querySelector(".usersNavLink-js");
+let editDeleteNavLink = document.querySelector(".editDeleteNavLink-js");
 
-//INPUT SEARCH
-let search=document.querySelector('#inputSearch');
+//Buttons
+let saveRentBtn = document.querySelector("#saveRentBtn");
+let saveEditUserBtn = document.querySelector("#saveEdit");
 
-
-
-//BUTTONS
-let usersViewBtn = document.querySelector("#usersView-btn");
-let rentViewBtn=document.querySelector("#rentView-btn");
-let saveRentbtn=document.querySelector("#saveRent-btn");
-let editUserbtn=document.querySelector('#editUserView-btn');
-let saveEditUser=document.querySelector('#editUser-btn');
-
-let editBtn=document.querySelectorAll(".edit-btn")
-//LISTENERS
+// Listeners
 window.addEventListener("hashchange", changeView);
-usersViewBtn.addEventListener("click", displayUsersView);
-rentViewBtn.addEventListener("click", displayRentViews);
-saveRentbtn.addEventListener("click",saveRent);
-editUserbtn.addEventListener("click",displayEditUserTable);
-search.addEventListener("input", searchTerm);
-saveEditUser.addEventListener("click",editUser)
+rentNavLink.addEventListener("click", displayRentView);
+saveRentBtn.addEventListener("click", saveNewRent);
+usersNavLink.addEventListener("click", displayUsers);
+editDeleteNavLink.addEventListener("click", createEditDeleteTable);
+saveEditUserBtn.addEventListener("click", editUser);
+searchField.addEventListener("input", searchUsers);
 
-//FUNCTIONS CALL
-createUsersTable(db);
-
-
-
-//DISPLAYS
-
-function displayEditUserForm(){
-  let id=this.getAttribute("data-id");
-  saveEditUser.setAttribute("data-id",id);
-  currentUser=db.find(user=>(user.id===id));
-  fillEditForm(currentUser);
-  location.hash="#editUserForm"
+//Search users
+function searchUsers() {
+  let searchTerm = this.value.toLowerCase();
+  let currentUsers = users.filter((user) => {
+    return (
+      user.name.toLowerCase().indexOf(searchTerm) !== -1 ||
+      user.lastName.toLowerCase().indexOf(searchTerm) !== -1 ||
+      user.idCardNumber.indexOf(searchTerm) !== -1 ||
+      user.phoneNumber.indexOf(searchTerm) !== -1 ||
+      user.startDate.indexOf(searchTerm) !== -1 ||
+      user.endDate.indexOf(searchTerm) !== -1
+    );
+  });
+  createUsersTable(currentUsers);
 }
+//Search users END
 
-function displayEditUserTable(){
+//Save edits
+function editUser(e) {
+  e.preventDefault();
+  let currentUser = users.find(
+    (user) => user.id === this.getAttribute("userID")
+  );
+
+  currentUser.idCardNumber = eIDcardNumberRent.value;
+  currentUser.name = efirstNameRent.value;
+  currentUser.lastName = elastNameRent.value;
+  currentUser.phoneNumber = ephoneNumberRent.value;
+  currentUser.roomType = eroomTypeSelectRent.value;
+  currentUser.startDate = estartDateRent.value;
+  currentUser.endDate = eendDateRent.value;
+
+  createUsersTable();
+  location.hash = "usersView";
+}
+//Save edits END
+
+//Create room options for edit form
+function createEditRoomTypeOptions(currentUser) {
   let content = "";
-  db.forEach((user) => {
-    content += `  <tr>
-    <td class="text-nowrap">${user.idCardNumber}</td>
-    <td class="text-nowrap">${user.user}</td>
-    <td class="text-nowrap">${user.phone}</td>
-    <td class="text-nowrap">${user.room}</td>
-    <td class="text-nowrap">${user.startDate}</td>
-    <td class="text-nowrap">${user.endDate}</td>
-    <td class="text-nowrap">
-    <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-danger delete-btn mr-2" data-id="${user.id}">Delete</button> 
-    <button type="button" class="btn btn-warning edit-btn" data-id="${user.id}">Edit</button>
-    </div>
-    </td>
-  </tr>`;
+  roomTypes.forEach((type) => {
+    content += `
+        <option ${
+          currentUser.roomType === type ? "selected" : ""
+        } value="${type}">${type}</option>
+        `;
+  });
+  eroomTypeSelectRent.innerHTML = content;
+}
+//Create room options for edit form END
+
+//Fill edit form
+function fillEditForm(currentUser) {
+  eIDcardNumberRent.value = currentUser.idCardNumber;
+  efirstNameRent.value = currentUser.name;
+  elastNameRent.value = currentUser.lastName;
+  ephoneNumberRent.value = currentUser.phoneNumber;
+  estartDateRent.value = currentUser.startDate;
+  eendDateRent.value = currentUser.endDate;
+  createEditRoomTypeOptions(currentUser);
+  saveEditUserBtn.setAttribute("userID", currentUser.id);
+}
+//Fill edit form END
+
+//Display edit view
+function displayEditView() {
+  let id = this.getAttribute("userID");
+  let currentUser = users.find((user) => user.id === id);
+
+  fillEditForm(currentUser);
+  location.hash = "editView";
+}
+//Display edit view END
+
+//Create table for edit and delete users
+function createEditDeleteTable() {
+  let content = "";
+  users.forEach((user) => {
+    content += ` 
+        <tr>
+        <td>${user.id}</td>
+        <td>${user.idCardNumber}</td>
+        <td>${user.name}</td>
+        <td>${user.lastName}</td>
+        <td>${user.phoneNumber}</td>
+        <td>${user.roomType}</td>
+        <td>${user.startDate}</td>
+        <td>${user.endDate}</td>
+        <td><button class="btn btn-warning editBtn-js"  userID=${user.id}>Edit</button> <button class="btn btn-danger deleteBtn-js"  userID=${user.id}>Delete</button></td>
+        </tr>`.trim();
+  });
+  editDeleteTable.innerHTML = content;
+
+  let deleteBtns = document.querySelectorAll(".deleteBtn-js");
+  let editBtns = document.querySelectorAll(".editBtn-js");
+
+  deleteBtns.forEach((btn, index) => {
+    btn.addEventListener("click", deleteUser);
+    editBtns[index].addEventListener("click", displayEditView);
+  });
+}
+//Create table for edit and delete users END
+
+//Delete user
+function deleteUser() {
+  let id = this.getAttribute("userID");
+
+  users = users.filter((user) => {
+    console.log(typeof user.id);
+    return user.id != id;
   });
 
-  tbodyEditUserView.innerHTML = content;
- 
+  createUsersTable();
+  location.hash = "usersView";
+}
+//Delete user END
 
-  let deleteBtns=document.querySelectorAll(".delete-btn");
-  let editBtns=document.querySelectorAll('.edit-btn');
+//SaveToLocalStorage
+function saveToLocalStorage() {
+  localStorage.db = JSON.stringify(users);
+}
+//SaveToLocalStorage END
 
-  deleteBtns.forEach((btn,index) => {
-    btn.addEventListener("click",deleteUser)
-    editBtns[index].addEventListener("click",displayEditUserForm)
+//Save new Rent
+function saveNewRent(e) {
+  e.preventDefault();
+  let newRent = {
+    id: idRent.value,
+    idCardNumber: IDcardNumberRent.value,
+    name: firstNameRent.value,
+    lastName: lastNameRent.value,
+    phoneNumber: phoneNumberRent.value,
+    roomType: roomTypeSelectRent.value,
+    startDate: startDateRent.value,
+    endDate: endDateRent.value,
+  };
+  let checkForm = Object.values(newRent).some((value) => value === "");
+
+  if (checkForm) {
+    alert("Fill all input fields");
+  } else {
+    resetRentForm();
+    users.push(newRent);
+    createUsersTable();
+    location.hash = "usersView";
+  }
+}
+//Save new Rent END
+
+//Reset Rent form
+function resetRentForm() {
+  idRent.value = "";
+  IDcardNumberRent.value = "";
+  firstNameRent.value = "";
+  lastNameRent.value = "";
+  phoneNumberRent.value = "";
+  roomTypeSelectRent.value = "";
+  startDateRent.value = "";
+  endDateRent.value = "";
+}
+//Reset Rent form END
+
+//Display users
+function displayUsers() {
+  createUsersTable();
+}
+//Display users END
+
+//Display rent view
+function displayRentView() {
+  createRoomTypeOptions();
+  idRent.value = generateUniqueID();
+}
+//Display rent view END
+
+//Create room options
+function createRoomTypeOptions() {
+  let content = "";
+  roomTypes.forEach((type) => {
+    content += `
+        <option value="${type}">${type}</option>
+        `;
+  });
+  roomTypeSelectRent.innerHTML = content;
+}
+//Create room options END
+
+//Create users table
+createUsersTable();
+location.hash = "usersView";
+
+function createUsersTable(currentUsers) {
+  let usersCopy = users;
+  if (currentUsers) {
+    usersCopy = currentUsers;
+  }
+  let content = "";
+  usersCopy.forEach((user) => {
+    content += ` 
+        <tr>
+        <td>${user.id}</td>
+        <td>${user.idCardNumber}</td>
+        <td>${user.name}</td>
+        <td>${user.lastName}</td>
+        <td>${user.phoneNumber}</td>
+        <td>${user.roomType}</td>
+        <td>${user.startDate}</td>
+        <td>${user.endDate}</td>
+    </tr>`.trim();
   });
 
-
+  usersTable.innerHTML = content;
 }
+//Create users table END
 
-function displayUsersView() {
-  createUsersTable(db);
-}
+//Generate random ID
+function generateUniqueID() {
+  let generateId;
+  let unique = false;
 
-function displayRentViews(){
-    createRoomTypeSelect();
-}
-
-
-//FUNCTIONS
-
-function editUser(){
-  let currentUser=db.find(user=>user.id===this.getAttribute("data-id"));
-  currentUser.idCardNumber=einputUserIDCard.value;
-  currentUser.user=einputUser.value;
-  currentUser.phone=einputUserPhone.value;
-  currentUser.room=eselectRoomType.value;
-  currentUser.startDate=einputStartDate.value;
-  currentUser.endDate=einputEndDate.value;
-
-  createUsersTable()
-  location.hash="#usersView";
-}
-
-
-
-function createEditRoomTypeSelect(currentUser){
-  let content="";
-  roomType.forEach(type => {
-      content+=`<option value="${type}"   ${(currentUser.room===type)? "selected":""}>${type}</option>`
-    
-  });
-
-  eselectRoomType.innerHTML=content;
-
-}
-
-function fillEditForm(currentUser){
-  einputUserIDCard.value=currentUser.idCardNumber;
-  einputUser.value=currentUser.user;
-  einputUserPhone.value=currentUser.phone;
-  createEditRoomTypeSelect(currentUser);
-  einputStartDate.value=currentUser.startDate;
-  einputEndDate.value=currentUser.endDate;
-}
-
-function searchTerm(){
-
-  let searchTerm=this.value;
-  let currentDB=db.filter(el=>{
-    return  el.user.indexOf(searchTerm) !== -1 ||
-    el.phone.indexOf(searchTerm) !== -1 ||
-     el.idCardNumber.indexOf(searchTerm) !== -1
-  })
-
-
-  
-createUsersTable(currentDB);
-
-}
-
-
-
-function save(){
-  localStorage.db = JSON.stringify(db);
-}
-
-
-
-function deleteUser(){
-  let id=this.getAttribute("data-id");
-  db=db.filter(user=>user.id!==id)
-  displayEditUserTable();
-
-}
-
-
-
-function saveRent(){
-    let saveObject= {
-        id: generateUserID(),
-        idCardNumber:inputUserIDCard.value,
-        user:inputUser.value ,
-        phone:inputUserPhone.value,
-        room: selectRoomType.value,
-        startDate: inputStartDate.value,
-        endDate:inputEndDate.value
-    }
-    db.push(saveObject);
-    resetForm();
-    createUsersTable(db);
-    location.hash="#usersView"
-}
-
-function generateUserID(){
-    let random;
-    let unique=false;
-    while(!unique){
-        unique=true;
-        random=Math.floor(Math.random()*99*db.length);
-        db.forEach(user =>{
-            if(parseInt(user.id)===random){
-                unique=false;
-            }
-        } );
-    }
-
-    
-    return random.toString();
-}
-
-
-function resetForm(){
-
-    inputUserIDCard.value="";
-    inputUser.value=""; 
-    inputUserPhone.value="";
-    selectRoomType.value="";
-    inputStartDate.value="";
-    inputEndDate.value="";
-}
-
-
-
-
-
-function createRoomTypeSelect(){
-    let content="";
-    roomType.forEach(type => {
-        content+=`<option value="${type}">${type}</option>`
-      
+  while (!unique) {
+    unique = true;
+    generateId = Math.floor(Math.random() * 10000);
+    users.forEach((user) => {
+      if (parseInt(user.id) === generateId) {
+        unique = false;
+      }
     });
-
-    selectRoomType.innerHTML=content;
-
+  }
+  return generateId.toString();
 }
+//Generate random ID END
 
+//Change view function
 function changeView() {
   let hash = location.hash.substring(1);
   views.forEach((view) => {
@@ -240,26 +292,4 @@ function changeView() {
     }
   });
 }
-
-function createUsersTable(currentDb) {
-  if(!currentDb){
-    currentDb=db;
-  }
-  let content = "";
-  currentDb.forEach((user) => {
-    content += `  <tr>
-    <td class="text-nowrap">${user.idCardNumber}</td>
-    <td class="text-nowrap">${user.user}</td>
-    <td class="text-nowrap">${user.phone}</td>
-    <td class="text-nowrap">${user.room}</td>
-    <td class="text-nowrap">${user.startDate}</td>
-    <td class="text-nowrap">${user.endDate}</td>
-  </tr>`;
-  });
-
-  tbodyUsersView.innerHTML = content;
-}
-
-
-
-
+//Change view function END
